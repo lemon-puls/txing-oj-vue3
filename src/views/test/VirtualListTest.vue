f2d1b6ade35d8ccce4f41710d35bcda2ffb776cf
lemon <13434615275@163.com>
2023-12-27 18:24:25 +0800

    引入了echarts

--- src/views/test/VirtualListTest.vue
+++ src/views/test/VirtualListTest.vue
@@ -19,15 +19,173 @@
     </svg>
     aSssadasD
     <SvgIcon icon="lock" />
+    <div ref="charts" style="width: 800px; height: 500px"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import VirtualList from "@/components/VirtualList";
-import { provide, ref } from "vue";
+import { onMounted, provide, ref } from "vue";
 import { throttle } from "lodash";
 import MsgItem from "@/views/test/MsgItem.vue";
 import SvgIcon from "@/icons/SvgIcon";
+import * as echarts from "echarts";
+
+let charts = ref();
+onMounted(() => {
+  // 获取节点
+  let mycharts = echarts.init(charts.value);
+  // mycharts.setOption({
+  //   tooltip: {
+  //     trigger: "axis",
+  //     axisPointer: { type: "cross" },
+  //   },
+  //   legend: {},
+  //   xAxis: [
+  //     {
+  //       type: "category",
+  //       axisTick: {
+  //         alignWithLabel: true,
+  //       },
+  //       data: [
+  //         "1月",
+  //         "2月",
+  //         "3月",
+  //         "4月",
+  //         "5月",
+  //         "6月",
+  //         "7月",
+  //         "8月",
+  //         "9月",
+  //         "10月",
+  //         "11月",
+  //         "12月",
+  //       ],
+  //     },
+  //   ],
+  //   yAxis: [
+  //     {
+  //       type: "value",
+  //       name: "降水量",
+  //       min: 0,
+  //       max: 250,
+  //       position: "right",
+  //       axisLabel: {
+  //         formatter: "{value} ml",
+  //       },
+  //     },
+  //     {
+  //       type: "value",
+  //       name: "温度",
+  //       min: 0,
+  //       max: 25,
+  //       position: "left",
+  //       axisLabel: {
+  //         formatter: "{value} °C",
+  //       },
+  //     },
+  //   ],
+  //   series: [
+  //     {
+  //       name: "降水量",
+  //       type: "bar",
+  //       yAxisIndex: 0,
+  //       data: [
+  //         6, 32, 70, 86, 68.7, 100.7, 125.6, 112.2, 78.7, 48.8, 36.0, 19.3,
+  //       ],
+  //     },
+  //     {
+  //       name: "温度",
+  //       type: "line",
+  //       smooth: true,
+  //       yAxisIndex: 1,
+  //       data: [
+  //         6.0, 10.2, 10.3, 11.5, 10.3, 13.2, 14.3, 16.4, 18.0, 16.5, 12.0, 5.2,
+  //       ],
+  //     },
+  //   ],
+  // });
+  mycharts.setOption({
+    // title: {
+    //   text: "水球图",
+    // },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: { type: "cross" },
+    },
+    legend: {},
+    xAxis: [
+      {
+        type: "category",
+        axisTick: {
+          alignWithLabel: true,
+        },
+        data: [
+          "1月",
+          "2月",
+          "3月",
+          "4月",
+          "5月",
+          "6月",
+          "7月",
+          "8月",
+          "9月",
+          "10月",
+          "11月",
+          "12月",
+        ],
+      },
+    ],
+    yAxis: [
+      // {
+      //   type: "value",
+      //   name: "降水量",
+      //   min: 0,
+      //   max: 250,
+      //   position: "right",
+      //   axisLabel: {
+      //     formatter: "{value} ml",
+      //   },
+      // },
+      {
+        type: "value",
+        name: "温度",
+        min: 0,
+        max: 25,
+        position: "left",
+        axisLabel: {
+          formatter: "{value} °C",
+        },
+      },
+    ],
+    series: [
+      // {
+      //   name: "降水量",
+      //   type: "bar",
+      //   yAxisIndex: 0,
+      //   data: [
+      //     6, 32, 70, 86, 68.7, 100.7, 125.6, 112.2, 78.7, 48.8, 36.0, 19.3,
+      //   ],
+      // },
+      {
+        name: "温度",
+        type: "line",
+        smooth: true,
+        yAxisIndex: 0,
+        radius: [0, "50%"],
+        data: [
+          6.0, 10.2, 10.3, 11.5, 10.3, 13.2, 14.3, 16.4, 18.0, 16.5, 12.0, 5.2,
+        ],
+      },
+    ],
+    // grid: {
+    //   left: 0,
+    //   right: 0,
+    //   top: 0,
+    //   bottom: 0,
+    // },
+  });
+});
 
 const chatMessageList = ref([
   {
